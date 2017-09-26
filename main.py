077a5ff3d1a88ca0304bd1128012f43308104a4a
bugra10ur <bugrasen1991@gmail.com>
2017-09-27 01:00:28 +0300

    ver1.01

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 from tsp_solver.greedy import solve_tsp as tsp
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
-from geopy.distance import vincenty
+from math import radians, cos, sin, asin, sqrt
+
 
 
 def make_dist_matrix(x, y):
@@ -24,12 +25,33 @@ def read_data(filename):
 
     return cities
 
+def vectorized_haversine(lat,lng):
+    lat = np.deg2rad(lat)
+    lng = np.deg2rad(lng)
+    dflat = lat[:,None] - lat
+    dflng = lng[:,None] - lng
+    d = np.sin(dflat/2)**2 + np.cos(lat[:,None])*np.cos(lat) * np.sin(dflng/2)**2
+    return 2 * 6371 * np.arcsin(np.sqrt(d))
+
+
+def km_to_second(matrix):
+    l=np.reshape(matrix, matrix.shape[0]*matrix.shape[1])
+    l=[x*3 if x <= 3 else x for x in l]
+    return np.reshape(l, (matrix.shape[0],matrix.shape[1]))
+
+
+def path_duration(time_matrix,path,delivery_time):
+    duration=0
+    for i in range(len(path)-1):
+        duration=duration+delivery_time+time_matrix[path[i],path[i+1]]
+    return duration
+
 
 cities = read_data('Coords')
 id    = np.array(cities)[:, 0]
 x     = np.array(cities)[:, 1]
 y     = np.array(cities)[:, 2]
-start = np.array(cities)[:,3]
+start = np.array(cities)[:, 3]
 end   = np.array(cities)[:, 4]
 #group = np.array(cities)[:, 5]
 
@@ -37,7 +59,6 @@ newcoloumn=[]
 temp=[]
 
 for row in cities:
-
    search = str(str(row[3]) + '-' + str(row[4]))
    index =temp.index(search) if search in temp else -1
    if index == -1:
@@ -55,9 +76,12 @@ matris = np.column_stack((id,x,y,start,end,group))
 for i in range(len(temp)):
     indices = group == i
     data = matris[indices,:]
-    distances = make_dist_matrix(data[:,1], data[:,2])
+    distances = vectorized_haversine(data[:,1], data[:,2])
     path = tsp(distances)
+    time=km_to_second(distances)
     print(path)
+    print("Toplam süre:"+"  "+str(path_duration(time,path,10))+" dk")
+
     plt.plot(data[path, 1], data[path, 2], list(colors.keys())[i])
     for k, txt in enumerate(data[path, 0]):
         plt.annotate(int(txt), (data[path[k], 1], data[path[k], 2]))
@@ -65,3 +89,6 @@ for i in range(len(temp)):
 
 plt.scatter(matris[:, 1], matris[:, 2], c=matris[:,5], cmap=plt.cm.Set1,edgecolor='k')
 plt.show()
+
+
+#birleştirme
